20c7a0beb0673e4c77f533489ebffe82e773b84c
SchwarzeFalke <autor.cvp303@gmail.com>
2018-10-15 14:37:52 -0500

    Finish structure of the project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,9 @@
+/**
+ * @Author: schwarze_falke
+ * @Date:   2018-10-15T13:21:59-05:00
+ * @Last modified by:   schwarze_falke
+ * @Last modified time: 2018-10-15T13:51:11-05:00
+ */
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
